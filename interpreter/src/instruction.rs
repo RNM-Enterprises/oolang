8c27f66a8d628313044dc8161ba4054890644937
Joe Harrison <josephharrison1@me.com>
2022-02-26 15:43:26 +0000

    added Ohs

--- interpreter/src/instruction.rs
+++ interpreter/src/instruction.rs
@@ -9,11 +9,25 @@ pub enum Instruction {
     WRITE, //Pop the top stack value, writing to stdout
     STORE, //Store the 2nd top stack value at the address specified by the top stack value
     LOAD, //Replace the top stack value with the value at the address specified by it
+    ADD, // Add the top two numbers from the stack
 }
+
+use Instruction::*;
 
 impl Instruction {
     pub fn from(c: &str) -> Option<Self> {
         match c {
+            "O" => Some(PUSH),    // Latin Capital letter O
+            "0" => Some(POP),     // Digit Zero
+            "Ǿ" => Some(INC),    // LATIN CAPITAL LETTER O WITH STROKE AND ACUTE
+            "Ꮻ" => Some(DEC),   //CHEROKEE LETTER WI
+            "𐍉" => Some(JNZ),  // GOTHIC LETTER OTHAL
+            "Ꝍ" => Some(JZ),    // LATIN CAPITAL LETTER O WITH LOOP
+            "⒪" => Some(READ),  //parenthesized latin small letter o
+            "ₒ" => Some(WRITE), //Latin Subscript Small Letter O
+            "⭕" => Some(ADD),   //Hollow Red Circle
+            "◎" => Some(LOAD),  // Bullseye
+            "⎈" => Some(STORE), // Helm Symbol
             _ => None,
         }
     }
